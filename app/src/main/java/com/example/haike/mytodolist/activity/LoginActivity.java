187bf25d46b6b4c8e1f32904b7bf58dd96b654dc
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-06-05 04:09:05 +0100

    update app

--- app/src/main/java/com/example/haike/mytodolist/activity/LoginActivity.java
+++ app/src/main/java/com/example/haike/mytodolist/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -29,6 +30,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         emailUser = findViewById(R.id.email);
         passUser = findViewById(R.id.password);
